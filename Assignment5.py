de86d2c47bb2739a786a6721f1090c0c646bceea
Ekankshi Sharma <39719829+ekankshi12@users.noreply.github.com>
2018-06-02 10:32:16 +0530

    Assignment 5

---
+++ Assignment5.py
@@ -0,0 +1,54 @@
+#Question 1
+#user_year=int(input("Enter year :"))
+#if(user_year%4==0 and user_year%100!=0 or user_year%400==0):
+#    print("Year is a leap year")
+#else:
+#    print("Year is not leap year")
+
+
+#Qusetion 2
+#length = int(input("Enter the length :"))
+#breadth = int(input("Enter the breadth :"))
+#if(length == breadth):
+#   print("Dimensions are of  a square")
+#else:
+#    print("Dimensions are of a rectangle")
+
+#Question 3
+#age1 = int(input("Enter the age of first person :"))
+#age2 = int(input("Enter the age of second person :"))
+#age3 = int(input("Enter the age of third person :"))
+#if(age1>age2 and age1>age3)
+#    print("First is oldest person")
+#else:
+ #   print("First is eldest person")
+#if(age2>age1 and age2>age3):
+   # print("Second is oldest person")
+#else:
+   # print("Second is eldest person")
+#if(age3>age1 and age3>age2):
+#    print("Third is oldest person")
+#else:
+#    print("Third is eldest person")
+
+#Question 4
+age = (input("Enter the age of  employee :"))
+sex = (input("Enter the sex of employee (M/F):"))
+marital_status = (input("Enter the marital status of employee :"))
+if(sex == F):
+    print("Employee is female so she will work in urban areas")
+elif(sex == M and age>20 and age<40):
+    print("Employee is male so he can work anywhere")
+elif(sex == M and age<40 and age<60):
+    print("Employee is male so he will work in urban areas")
+else:
+    print("Error in input")
+
+ #Question 5
+#quantity = int(input("Enter quantity: "))
+#if(quantity*100 > 1000):
+#  print("Cost is",((quantity*100)-(.1*quantity*100)))
+#else:
+#  print("Cost is",quantity*100)
+
+
