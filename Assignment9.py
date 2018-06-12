8973918f5147c1af592a938185aa10340896aabf
Ekankshi Sharma <39719829+ekankshi12@users.noreply.github.com>
2018-06-12 18:04:48 +0530

    Assignment 9

--- Assignment9.py
+++ Assignment9.py
@@ -18,8 +18,8 @@ from threading import Thread
 def sleep(x):
      print(x)
      time.sleep(1)
-for x in range(10):
-    t=Thread(target=sleep,args=(x))
+for x in range(11):
+    t=Thread(target=sleep,args=(x,))
     t.start()
 
 
@@ -27,13 +27,16 @@ for x in range(10):
 import time
 import threading
 from threading import Thread
+def thread_process(i , b):
+	    time.sleep(b)
+	    print("Thread %i." % (i))
 
 list = [1,2,3,4,5]
-def thread_process():
-    time.sleep(2)
-    print(list)
-t=Thread(target=thread_process,args=())
-t.start()
+b=2
+for i in list:
+     t=Thread(target=thread_process,args=(i,b,))
+     t.start()
+     b+= 2
 
 
 #Question 4
@@ -42,9 +45,14 @@ import threading
 from threading import Thread
 
 def factorial(n):
-    if n < 1:
+    if n == 1:
         return 1
     else:
-        return n * factorial(n - 1)
-t=Thread(target=factorial,args=(n))
+        f = n * factorial(n - 1)
+        return f
+        print("The factorial is:",f)
+n = int(input("Enter the number"))
+t=Thread(target=factorial,args=(n,))
 t.start()
+
+*** END****
