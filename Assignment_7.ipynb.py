a9b8d4dc5646412fc51534910a7d9378f7e1521e
Ekankshi Sharma <39719829+ekankshi12@users.noreply.github.com>
2018-06-22 22:09:19 +0530

    Assignment_7.ipynb

--- Assignment_7.ipynb.py
+++ Assignment_7.ipynb.py
@@ -30,4 +30,11 @@ s = np.sum(res, 0)
 print(s)
 
 
-
+# Question 4
+import numpy as np
+A = np.random.rand(10,1)
+print(A)
+def func(x):
+    return (1 / (1 + np.exp(-x)))
+result = np.apply_along_axis(func, 0, A)
+print(result)
